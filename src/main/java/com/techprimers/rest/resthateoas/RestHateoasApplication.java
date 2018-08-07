8f0fe939fcec28ce4af78cad837779b6b5f7d5f1
Townsend <Shane.Townsend@libertyinsurance.ie>
2018-08-07 15:17:23 +0100

    Commit message

--- src/main/java/com/techprimers/rest/resthateoas/RestHateoasApplication.java
+++ src/main/java/com/techprimers/rest/resthateoas/RestHateoasApplication.java
@@ -2,11 +2,17 @@ package com.techprimers.rest.resthateoas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class RestHateoasApplication {
-
-	public static void main(String[] args) {
+public class RestHateoasApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(RestHateoasApplication.class);
+    }
+	
+    public static void main(String[] args) {
 		SpringApplication.run(RestHateoasApplication.class, args);
 	}
 }
